e0982149c141fae128c3f21c9daf18721d163ca2
vindarel <ehvince@mailz.org>
2017-01-09 00:00:30 +0100

    define test suites

--- test-cl-s.lisp
+++ test-cl-s.lisp
@@ -11,16 +11,22 @@
 (setf prove:*enable-colors* t)
 (plan nil)
 
-(is "rst " (s-trim-left "   rst "))
-(is " rst" (s-trim-right " rst   "))
-(is "rst" (s-trim "  rst  "))
+(subtest "Trim"
+  (is "rst " (s-trim-left "   rst "))
+  (is " rst" (s-trim-right " rst   "))
+  (is "rst" (s-trim "  rst  "))
+  )
 
 
-(is "foo bar baz" (s-join " " '("foo" "bar" "baz")))
-(is "foo+++bar+++baz" (s-join "+++" '("foo" "bar" "baz")))
+(subtest "Join"
+  (is "foo bar baz" (s-join " " '("foo" "bar" "baz")))
+  (is "foo+++bar+++baz" (s-join "+++" '("foo" "bar" "baz")))
+  )
 
-;; split
-(is '("foo" "bar") (s-split " " "foo bar"))
+(subtest "Split"
+  (is '("foo" "bar") (s-split " " "foo bar"))
+  (is '("foo" "bar") (s-split "\\+" "foo+bar"))
+  )
 
 ;; prove end
 (finalize)
